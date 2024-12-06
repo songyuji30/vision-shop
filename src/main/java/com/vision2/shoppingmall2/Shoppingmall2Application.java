40df212524fd0923b5676288ce2d00bfe75e2074
Uuuug <songyuji30@naver.com>
2024-12-06 23:25:44 +0900

    iniy

---
+++ src/main/java/com/vision2/shoppingmall2/Shoppingmall2Application.java
@@ -0,0 +1,13 @@
+package com.vision2.shoppingmall2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Shoppingmall2Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Shoppingmall2Application.class, args);
+  }
+
+}
---
+++ src/test/java/com/vision2/shoppingmall2/Shoppingmall2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vision2.shoppingmall2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Shoppingmall2ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
